b8332af3fe63bb4d2b2b563becaf0a4fa36d39a9
j-kanuch <joshua.d.kanuch@vumc.org>
2020-07-01 14:10:13 -0500

    Removed override biobank prefix; setting in manifest handler.

--- rdr_service/genomic/genomic_biobank_manifest_handler.py
+++ rdr_service/genomic/genomic_biobank_manifest_handler.py
@@ -26,7 +26,7 @@ _MAX_INPUT_AGE = datetime.timedelta(hours=24)
 # sample suffix: -v12019-04-05-00-30-10.csv
 _RESULT_CSV_FILE_SUFFIX_LENGTH = 26
 
-BIOBANK_ID_PREFIX = "T"
+BIOBANK_ID_PREFIX = "A" if config.GAE_PROJECT == "all-of-us-rdr-prod" else "T"
 
 
 def process_genomic_manifest_result_file_from_bucket():
@@ -160,7 +160,7 @@ def create_and_upload_genomic_biobank_manifest_file(genomic_set_id, timestamp=No
       ORDER BY id
     """
     query_params = {"genomic_set_id": genomic_set_id,
-                    "prefix": config.getSetting(config.BIOBANK_ID_PREFIX),
+                    "prefix": BIOBANK_ID_PREFIX,
                     "aw0_ready_state": int(GenomicWorkflowState.AW0_READY)}
     exporter.run_export(result_filename, export_sql, query_params)
 
